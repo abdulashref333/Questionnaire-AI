fcdfaa07d4c023fd61f095684c01ad6f6b065f53
Abdulrahman Ashraf <abdelrahman.ashraf@espace.com.eg>
2024-02-22 01:49:57 +0200

    update form style

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,22 @@ import Input from "./components/Form/Input";
 export default function Home() {
   const [input, setInput] = useState("")
 
-  const handleOnSubmit = (event: FormEvent<HTMLFormElement>) =>{
+  const handleOnSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
   }
 
   return (
-    <form className='flex content-center justify-center flex-col m-10' onSubmit={handleOnSubmit}>
-      <Input label="Enter a phrase to make it better" type="text" value={input} onChange={(e)=>{setInput(e.target.value)}}/>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Generate</button>
+    <form className='flex content-center items-center justify-between m-10' onSubmit={handleOnSubmit}>
+      <Input
+        label="Enter a phrase to make it better"
+        type="text"
+        value={input} onChange={(e) => { setInput(e.target.value) }}
+      />
+      <button
+        className="mx-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-4 rounded inline-flex items-center text-center"
+      >
+        Generate
+      </button>
     </form>
   )
 }
