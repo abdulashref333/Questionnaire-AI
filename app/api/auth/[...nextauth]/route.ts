1a05b1f10874cf9eae3f24895f282cedbd50f7f4
Esraa Mohamed <59034378+esraa00@users.noreply.github.com>
2024-02-22 01:31:12 +0200

    Revert "Add langchain api endpoint"

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,35 +1,32 @@
-import NextAuth, { RequestInternal, User } from 'next-auth'
+import NextAuth from 'next-auth'
 import CredentialsProvider from "next-auth/providers/credentials"
 import { findOne } from '@/lib/services/user.service';
-
-export const authOptions = {
-  providers: [
+const handler = NextAuth({  providers: [
     CredentialsProvider({
       // The name to display on the sign-in form (e.g. 'Sign in with...')
       name: 'Credentials',
       credentials: {
         email: { label: "Email", type: "text", placeholder: "jsmith@example.com" },
-        password: { label: "Password", type: "password" }
+        password: {  label: "Password", type: "password" }
       },
-      authorize: async (
-        credentials: Record<"email" | "password", string> | undefined, 
-        req: Pick<RequestInternal, "body" | "query" | "headers" | "method">
-      ): Promise<User | null> => {
+      async authorize(credentials){
         // Add logic here to look up the user from the credentials supplied
-        if(!credentials?.email) return Promise.resolve(null);
-
+        if(!credentials?.email) return false
         const user = await findOne(credentials.email)
         if (user) {
           // Any object returned will be saved in `user` property of the JWT
-          return Promise.resolve({id: user.id, ...user})
+          return Promise.resolve(user)
         } else {
           // If you return null or false then the credentials will be rejected
           return Promise.resolve(null)
+          // You can also Reject this callback with an Error or with a URL:
+          // return Promise.reject(new Error('error message')) // Redirect to error page
+          // return Promise.reject('/path/to/redirect')        // Redirect to a URL
         }
       }
     }),
+    // ...add more providers here
   ],
-}
-
-const handler = NextAuth(authOptions);
+  // ...rest of the NextAuth configuration
+});
 export { handler as GET, handler as POST }
