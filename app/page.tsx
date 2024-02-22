138269a16a6f603b8f0881842e5fec1011050c2b
Abdulrahman Ashraf <abdelrahman.ashraf@espace.com.eg>
2024-02-22 03:09:44 +0200

    add Form component and integrate with api

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,22 @@
 "use client"
-import { FormEvent, useState } from "react"
-import Input from "./components/Form/Input";
+import { useState } from "react"
+import React from 'react'
+import { Form } from "./components/Form/Form";
+
 
 export default function Home() {
-  const [input, setInput] = useState("")
 
-  const handleOnSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-  }
-
+  const [response, setResponse] = useState("");
   return (
-    <form className='flex content-center items-center justify-between m-10' onSubmit={handleOnSubmit}>
-      <Input
-        label="Enter a phrase to make it better"
-        type="text"
-        value={input} onChange={(e) => { setInput(e.target.value) }}
-      />
-      <button
-        className="mx-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-4 rounded inline-flex items-center text-center"
-      >
-        Generate
-      </button>
-    </form>
+    <>
+      <Form setResponse={setResponse}></Form>
+      {
+        response && (
+          < div className="flex flex-row justify-center my-5">
+            <h1 className="text-xl text-white">{response}</h1>
+          </div >
+        )
+      }
+    </>
   )
 }
