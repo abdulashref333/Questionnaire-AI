6d151d678700003189d03ac5fe1b3e7c106db077
Abdulrahman Ashraf <abdelrahman.ashraf@espace.com.eg>
2024-02-22 22:00:53 +0200

    Update app to use local db

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,25 +5,21 @@ import { findOne } from '@/lib/services/user.service';
 export const authOptions = {
   providers: [
     CredentialsProvider({
-      // The name to display on the sign-in form (e.g. 'Sign in with...')
       name: 'Credentials',
       credentials: {
         email: { label: "Email", type: "text", placeholder: "jsmith@example.com" },
         password: { label: "Password", type: "password" }
       },
       authorize: async (
-        credentials: Record<"email" | "password", string> | undefined, 
+        credentials: Record<"email" | "password", string> | undefined,
         req: Pick<RequestInternal, "body" | "query" | "headers" | "method">
       ): Promise<User | null> => {
-        // Add logic here to look up the user from the credentials supplied
-        if(!credentials?.email) return Promise.resolve(null);
+        if (!credentials?.email) return Promise.resolve(null);
 
         const user = await findOne(credentials.email)
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-          return Promise.resolve({id: user.id, ...user})
+        if (user && user.password === credentials.password) {
+          return Promise.resolve({ id: user.id, ...user })
         } else {
-          // If you return null or false then the credentials will be rejected
           return Promise.resolve(null)
         }
       }
